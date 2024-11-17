6cc1238e776f3560a59e54f6acd71f136b91c2bd
doz34 <dorian.ciet@gmail.com>
2024-11-17 19:04:15 +0100

    feat: implemented simple dice roll

--- dice_simulator.py
+++ dice_simulator.py
@@ -1,2 +1,8 @@
 # dice_simulator.py
-"""Dice simulator."""
+import random
+
+def roll_die():
+    return random.randint(1, 6)
+
+print("Dice simulator")
+print(f"Rolled: {roll_die()}") # The function was not called in previous code!
