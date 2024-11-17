3227ee4cf1c5dddbba7aedb248f4d3cd87f8d01b
doz34 <dorian.ciet@gmail.com>
2024-11-17 19:10:34 +0100

    feat: multiple dice rolls implemented, modified after conflict

--- dice_simulator.py
+++ dice_simulator.py
@@ -4,6 +4,11 @@ import random
 def roll_die(sides=6):
     return random.randint(1, sides)
 
-print("Dice simulator")
+def roll_multiple_dice(num_dice, sides=6):
+    return [roll_die(sides) for _ in range(num_dice)]
+
+print("Dice Simulator")
 sides = int(input("Enter the number of sides on the die: "))
-print(f"Rolled: {roll_die(sides)}")
+num_dice = int(input("Enter the number of dice to roll: "))
+results = roll_multiple_dice(num_dice, sides)
+print(f"Rolled: {results}")
