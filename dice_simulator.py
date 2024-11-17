8daa429d7b8c8c07c5a9b5ade77e3581673129f9
doz34 <dorian.ciet@gmail.com>
2024-11-17 19:08:23 +0100

    feat: customised dice rolls

--- dice_simulator.py
+++ dice_simulator.py
@@ -1,8 +1,9 @@
 # dice_simulator.py
 import random
 
-def roll_die():
-    return random.randint(1, 6)
+def roll_die(sides=6):
+    return random.randint(1, sides)
 
 print("Dice simulator")
-print(f"Rolled: {roll_die()}") # The function was not called in previous code!
+sides = int(input("Enter the number of sides on the die: "))
+print(f"Rolled: {roll_die(sides)}")
