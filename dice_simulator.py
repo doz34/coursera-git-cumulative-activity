fcb73806820409d12e619d6d4c911fefd8925cc4
doz34 <dorian.ciet@gmail.com>
2024-11-17 19:25:10 +0100

    feat: implemented save results to a file

--- dice_simulator.py
+++ dice_simulator.py
@@ -7,8 +7,18 @@ def roll_die(sides=6):
 def roll_multiple_dice(num_dice, sides=6):
     return [roll_die(sides) for _ in range(num_dice)]
 
+def save_results_to_file(results, filename="results.txt"):
+    with open(filename, "w") as file:
+        file.write("Rolled dice results:\n")
+        file.write(", ".join(map(str, results)))
+        file.write("\n")
+
 print("Dice Simulator")
 sides = int(input("Enter the number of sides on the die: "))
 num_dice = int(input("Enter the number of dice to roll: "))
 results = roll_multiple_dice(num_dice, sides)
 print(f"Rolled: {results}")
+save_results_to_file(results)
+print("Results saved to results.txt")
+print("End of simulation")
+print("Thank you for using the simulator")
